7316d858d43872bdb275365619d9dc7f04a35284
amirhrb <amirrabi70@gmail.com>
2023-12-09 23:06:29 +0330

    add loginHandler Fn

---
+++ src/utils/loginHandler.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+// fn
+import handleLocalStorage from './handleLocalStorage'
+
+const API_URL = import.meta.env.VITE_API_URL
+
+const loginHandler = async ({ email, password }: { email: string; password: string }) => {
+  const data = JSON.stringify({
+    user: {
+      email,
+      password
+    }
+  })
+  const headers = {
+    'Content-Type': 'application/json',
+    'X-Requested-With': 'XMLHttpRequest'
+  }
+  const userData = await axios
+    .post(`${API_URL}/users/login`, data, { headers })
+    .then(({ data }) => {
+      if (data.user) handleLocalStorage('token', data.user.token)
+      return { data, status: 'success', message: 'Login successfuly' }
+    })
+    .catch((error) => {
+      if (error.response.status === 403) {
+        return { status: 'failed', message: 'Login Failed! User name and/or Password is invalid' }
+      }
+    })
+  return userData
+}
+export default loginHandler
